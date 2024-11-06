002ab04b6765ec23b251cfe7c36ee1db23461fd6
Reacher <macryze@icloud.com>
2024-11-06 15:21:11 +0800

    Add main method to LastPencil class
    This commit adds a main method to the LastPencil class in the stage_1 package. The main method currently prints a line of pencils and a prompt indicating it is the user's turn. This sets up the basic interactive structure for future development.

--- src/stage_1/LastPencil.java
+++ src/stage_1/LastPencil.java
@@ -1,4 +1,8 @@
 package stage_1;
 
 public class LastPencil {
+    public static void main(String[] args) {
+        System.out.println("||||||||");
+        System.out.println("Your turn!");
+    }
 }
